7404873ba0e37dcd045b8912e18725cce650cf3a
Lucas Costa Campos <lucasnick33@gmail.com>
2018-01-14 11:45:58 -0300

    Produto

---
+++ src/java/produto/Produto.java
@@ -0,0 +1,93 @@
+package produto;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Produto implements Serializable {
+    
+    @Id
+    private Long id;
+    private String nome;
+    private String marca;
+    private String descricao;
+    private Double precoCompra;
+    private Double precoVenda;
+    private Double quantidade;
+    private String categoria;
+    private Double porcentagemDesconto;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPrecoCompra() {
+        return precoCompra;
+    }
+
+    public void setPrecoCompra(Double precoCompra) {
+        this.precoCompra = precoCompra;
+    }
+
+    public Double getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(Double precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    public Double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Double quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Double getPorcentagemDesconto() {
+        return porcentagemDesconto;
+    }
+
+    public void setPorcentagemDesconto(Double porcentagemDesconto) {
+        this.porcentagemDesconto = porcentagemDesconto;
+    }
+    
+}
