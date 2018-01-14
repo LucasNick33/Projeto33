1d87b29b4e3bfb27b7b7876891890bb6566ac0b1
Lucas Costa Campos <lucasnick33@gmail.com>
2018-01-14 13:10:16 -0300

    Retirada de desconto na venda
    Já existe esse campo no Usiário

--- src/java/rn/Permissao.java
+++ src/java/rn/Permissao.java
@@ -5,7 +5,6 @@ public enum Permissao {
     CADASTRO_USUARIO(1, "Cadastro de usuário"),
     CADASTRO_PRODUTO(2, "Cadastro de produto"),
     CADASTRO_CLIENTE(3, "Cadastro de cliente"),
-    DESCONTO_VENDA(4, "Desconto na venda"),
     DESCONTO_ITEM_VENDA(5, "Desconto no item de venda");
     
     private final Integer id;
