8c34abcabc4ae67fd12f98c0bded44d5d8221627
Lucas Costa Campos <lucasnick33@gmail.com>
2018-01-14 11:57:23 -0300

    ItemVenda

---
+++ src/java/venda/ItemVenda.java
@@ -0,0 +1,84 @@
+package venda;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class ItemVenda implements Serializable {
+    
+    @Id
+    private Long id;
+    private Long idProduto;
+    private Long idVenda;
+    private Double precoCompra;
+    private Double precoVenda;
+    private Double quantidade;
+    private Double porcentagemDesconto;
+    private Boolean sugestao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(Long idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public Long getIdVenda() {
+        return idVenda;
+    }
+
+    public void setIdVenda(Long idVenda) {
+        this.idVenda = idVenda;
+    }
+
+    public Double getPrecoCompra() {
+        return precoCompra;
+    }
+
+    public void setPrecoCompra(Double precoCompra) {
+        this.precoCompra = precoCompra;
+    }
+
+    public Double getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(Double precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    public Double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Double quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getPorcentagemDesconto() {
+        return porcentagemDesconto;
+    }
+
+    public void setPorcentagemDesconto(Double porcentagemDesconto) {
+        this.porcentagemDesconto = porcentagemDesconto;
+    }
+
+    public Boolean getSugestao() {
+        return sugestao;
+    }
+
+    public void setSugestao(Boolean sugestao) {
+        this.sugestao = sugestao;
+    }
+    
+}
