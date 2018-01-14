58ff0c33e219f8f9c7a6be900ba411b573bcd3af
Lucas Costa Campos <lucasnick33@gmail.com>
2018-01-14 11:30:36 -0300

    Usuario

---
+++ src/java/usuario/Usuario.java
@@ -0,0 +1,67 @@
+package usuario;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario implements Serializable {
+    
+    @Id
+    private Long id;
+    private String nome;
+    private String CPF;
+    private String senha;
+    private Timestamp dataNascimento;
+    private String permissoes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Timestamp getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Timestamp dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getPermissoes() {
+        return permissoes;
+    }
+
+    public void setPermissoes(String permissoes) {
+        this.permissoes = permissoes;
+    }
+    
+}
