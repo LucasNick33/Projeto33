8cdfc224a282f786808fa1c60b2ea48fde4dd321
Lucas Costa Campos <lucasnick33@gmail.com>
2018-01-19 10:36:40 -0300

    Cookie de Sessao

--- src/java/rn/Sessao.java
+++ src/java/rn/Sessao.java
@@ -15,19 +15,23 @@ import dao.ProdutoDao;
 import dao.UsuarioDao;
 import dao.VendaDao;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
+import java.util.UUID;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @ManagedBean
 public class Sessao {
-    
+
     private static final List<Sessao> SESSOES;
-    
-    static{
+
+    static {
         SESSOES = new ArrayList<>();
     }
-    
+
     private RNVenda rnVenda;
     private String id;
     private VariaveisGlobais variaveisGlobais;
@@ -48,70 +52,88 @@ public class Sessao {
         this.variaveisGlobais = variaveisGlobais;
     }
 
-    public String getId() {
-        return id;
+    private void setIdSessao() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        Cookie cookie = new Cookie("idSessao", id);
+        cookie.setMaxAge(3600);
+        ((HttpServletResponse) context.getExternalContext().getResponse()).addCookie(cookie);
     }
 
-    public void setId(String id) {
-        this.id = id;
+    private String getIdSessao() {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) facesContext.getExternalContext().getRequest();
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("idSessao")) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return "";
     }
 
-    public void getSessao(String id){
-        if(id != null && !id.isEmpty()){
-            for(Sessao s : SESSOES){
-                if(s.id.equals(id)){
+    public void getSessao() {
+        String idSessao = getIdSessao();
+        
+        if (idSessao != null && !idSessao.isEmpty()) {
+            for (Sessao s : SESSOES) {
+                if (s.id.equals(idSessao)) {
                     copiar(s);
                     return;
                 }
             }
         }
+        
         novaSessao();
     }
-    
-    private void novaSessao(){
-        this.id = String.valueOf(Calendar.getInstance().getTimeInMillis());
-        
+
+    private void novaSessao() {
+        this.id = UUID.randomUUID().toString();
+
         variaveisGlobais = new VariaveisGlobais();
         variaveisGlobais.setUsuario(new Usuario());
-        
+
         UsuarioDao usuarioDao = new UsuarioDao(variaveisGlobais);
         usuarioDao.setUsuario(variaveisGlobais.getUsuario());
         variaveisGlobais.setUsuarioDao(usuarioDao);
-        
+
         ClienteDao clienteDao = new ClienteDao(variaveisGlobais);
         clienteDao.setCliente(new Cliente());
         variaveisGlobais.setClienteDao(clienteDao);
-        
+
         ProdutoDao produtoDao = new ProdutoDao(variaveisGlobais);
         produtoDao.setProduto(new Produto());
         variaveisGlobais.setProdutoDao(produtoDao);
-        
+
         VendaDao vendaDao = new VendaDao(variaveisGlobais);
         vendaDao.setVenda(new Venda());
         variaveisGlobais.setVendaDao(vendaDao);
-        
+
         ItemVendaDao itemDao = new ItemVendaDao();
         itemDao.setItem(new ItemVenda());
         variaveisGlobais.setItemDao(itemDao);
-        
+
         PagamentoDao pagamentoDao = new PagamentoDao();
         pagamentoDao.setPagamento(new Pagamento());
         variaveisGlobais.setPagamentoDao(pagamentoDao);
-        
+
         EstoqueDao estoqueDao = new EstoqueDao(variaveisGlobais);
         estoqueDao.setEstoque(new Estoque());
         estoqueDao.getEstoque().setNome(variaveisGlobais.getUsuario().getEstoque());
         variaveisGlobais.setEstoqueDao(estoqueDao);
-        
+
         rnVenda = new RNVenda(variaveisGlobais);
-        
+
         SESSOES.add(this);
+        
+        setIdSessao();
     }
-    
-    private void copiar(Sessao s){
+
+    private void copiar(Sessao s) {
         this.id = s.id;
         this.variaveisGlobais = s.variaveisGlobais;
         this.rnVenda = s.rnVenda;
     }
-    
+
 }
